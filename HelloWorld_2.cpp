55909731af7237ccda461a374ae2d377f5e4dd4f
Andrey Sherbakov <asch1998@gmail.com>
2020-04-16 01:37:31 +0300

    повторил действия преподователя

--- HelloWorld_2.cpp
+++ HelloWorld_2.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+//Вызывает функцию std::cout
+void print()
+{
+    //Потому что хотим использовать стандартную библиотеку для печати в консоль
+    //и не использовать какие либо другие библиотеки.
+    std::cout << "Hello Skillbox!/n";
+}
+/*
+ *Присваеваем значения переменным и печатаем hello world!
+ */
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+
+
+    int mult = x * y;
+    int random = 1002;
+
+
+    std::cout << random;
+
+   
 }
